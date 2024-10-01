d66cdc4548f66644e2229b973a5682d8da3b5de5
Bill Mania <bill@manialabs.us>
2024-10-01 05:30:44 -0500

    Added another test to main.c.
    https://github.com/billmania/foam_cutter/issues/1

--- configs/mocks/main.c
+++ configs/mocks/main.c
@@ -11,18 +11,22 @@ int main(void)
     KINEMATICS_FORWARD_FLAGS fflags = 0UL;
     KINEMATICS_INVERSE_FLAGS iflags = 0UL;
 
-    pos.tran.x = 0.0;
-    pos.tran.y = 0.0;
-    function_result = kinematicsInverse(&pos, &j, &iflags, &fflags);
-    if (function_result != 0) {
-        printf("kinematicsInverse returned %d\n", function_result);
-    } else {
-        printf(
-            "kinematicsInverse: Axes: %0.3f, %0.3f Joints: %0.3f, %0.3f\n",
-            pos.tran.x,
-            pos.tran.y,
-            j[0],
-            j[1]
-            );
+    for (double x = 0.0; x <= 100.0; x += 10.0) {
+        for (double y = 0.0; y <= 100.0; y += 10.0) {
+            pos.tran.x = x;
+            pos.tran.y = y;
+            function_result = kinematicsInverse(&pos, &j, &iflags, &fflags);
+            if (function_result != 0) {
+                printf("kinematicsInverse returned %d\n", function_result);
+            } else {
+                printf(
+                    "kinematicsInverse: Axes: %0.3f, %0.3f Joints: %0.3f, %0.3f\n",
+                    pos.tran.x,
+                    pos.tran.y,
+                    j[0],
+                    j[1]
+                    );
+            }
+        }
     }
 }
